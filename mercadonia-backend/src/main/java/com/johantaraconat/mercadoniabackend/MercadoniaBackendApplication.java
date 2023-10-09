a6007cc7fe029046271d52b6516b9f25841024db
Jtaraconat <johan.taraconat@gmail.com>
2023-10-09 11:12:58 +0200

    First init app architecture ready
    packages are created

---
+++ mercadonia-backend/src/main/java/com/johantaraconat/mercadoniabackend/MercadoniaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.johantaraconat.mercadoniabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoniaBackendApplication.class, args);
+	}
+}
---
+++ mercadonia-backend/src/test/java/com/johantaraconat/mercadoniabackend/MercadoniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johantaraconat.mercadoniabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
